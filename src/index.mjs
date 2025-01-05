9e48d4256941c3ce6d1cc1fa70d07fe6d04bb516
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-01-05 12:04:33 +0600

    example of route params

--- src/index.mjs
+++ src/index.mjs
@@ -10,12 +10,32 @@ app.get("/", (req, res) => {
     res.status(201).send({msg: "Hi"}) // headers
 })
 
+const mockUsers = [
+    {id: 1, username: "collin", displayName: "Collin"},
+    {id: 2, username: "jimmy", displayName: "Jimmy"},
+    {id: 3, username: "hope", displayName: "Hope"},
+]
+
 app.get("/api/users", (req, res) => {
-    res.send([
-        {id: 1, username: "collin", displayName: "Collin"},
-        {id: 2, username: "jimmy", displayName: "Jimmy"},
-        {id: 3, username: "hope", displayName: "Hope"},
-    ])
+    res.send(mockUsers)
+})
+
+// Route params
+app.get("/api/users/:id", (req, res) => {
+    console.log(req.params);
+    const parsedId = parseInt(req.params.id)
+
+    if(isNaN(parsedId)) {
+        return res.status(400).send({msg: "Invalid id"})
+    }
+
+    const findUser = mockUsers.find((user) => user.id === parsedId)
+
+    if(!findUser) {
+        return res.status(404).send('User not found')
+    }
+
+    return res.send(findUser)
 })
 
 app.get("/api/products", (req, res) => {
